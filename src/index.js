5b99284d2fb78a4128efbd9b3e78fd8b16978f87
Gustavo Brandão <gustavo1731@hotmail.com>
2025-05-11 16:53:06 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,40 @@
+const fs = require('fs');
+
 // Primeiro problema é receber o texto:
 
 const caminhoArquivo = process.argv;
 const link = caminhoArquivo[2];
 
-console.log(link);
+fs.readFile(link, 'utf-8', (erro, texto) => {
+    qubraEmPagrafos(texto);
+    //verificaPalavrasDuplicadas(texto);
+});
 
-// Transformar texto em uma string:
+
+// criar um array com as palavras.
+// contar as ocorrências.
+// montar um objeto com o resultado.
+
+// {
+//    "web": 5,
+//    "computador": 4,
+// }
+
+function qubraEmPagrafos(texto) {
+    const paragrafos = texto. toLowerCase().split('\n');
+    const contagem = paragrafos.map((paragrafo) => {
+        return verificaPalavrasDuplicadas(paragrafo);
+    })
+    console.log(contagem);
+}
+
+function verificaPalavrasDuplicadas(texto) {
+    const listaPalavras = texto.split(' ');
+    const resultado = {};
+
+    // objeto[propriedade] = valor;
+    listaPalavras.forEach(palavra => {
+        resultado[palavra] = (resultado[palavra] || 0) + 1
+    })
+    return resultado;
+}
